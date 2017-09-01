314c6add509e65df8f61e4ae6b8b1b0011f460e2
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-09-01 08:24:36 -0500

    Assessment1

---
+++ src/standards/Test1.java
@@ -0,0 +1,13 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Wanna hear a joke? Well my friend's bakery got burned down");
+		JOptionPane.showMessageDialog(null, "His business is toasted now.");
+	}
+
+}
---
+++ src/standards/Test3.java
@@ -0,0 +1,12 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Today is west varisty first football home game of the year.");
+		System.out.println("They'll be facing of the central blue devils.");}
+
+}
